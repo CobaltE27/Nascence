d806ea577585249045254ca135edb7091b97ee45
CobaltE27 <potassiumE19@gmail.com>
2024-02-06 22:10:11 -0700

    Created a puppeteer for the character. Tagged Character and Enemy as such. Did some file rearranging.

---
+++ Assets/Entities/Character/EnemyPuppeteer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPuppeteer : MonoBehaviour
+{
+    private HashSet<GameObject> puppets;
+    private DebugLogger dLog;
+
+
+	void Start()
+    {
+        dLog = new DebugLogger();
+		#region BebugLogger Keys
+		dLog.loggableSystems = new Dictionary<string, bool>
+		{
+			{ "CheckForNewPuppets", true }
+		};
+		#endregion
+
+		StartCoroutine(CheckForNewPuppets());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator CheckForNewPuppets()
+    {
+        while (true)
+        {
+            List<GameObject> candidates = new List<GameObject>(FindObjectsOfType<GameObject>());
+            foreach (GameObject candidate in candidates)
+            {
+                //this would be the place to let enemies refuse to be added if that's ever a thing
+            }
+
+            puppets.UnionWith(candidates); //automatically avoids duplicates
+
+            dLog.Log("Finished looking for candidates", "CheckForNewPuppets");
+            yield return new WaitForSeconds(0.5f); 
+        }
+    }
+}
