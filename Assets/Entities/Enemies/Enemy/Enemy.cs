7320a8eba9cef7c7f14dab7a31463d6fcb81796d
CobaltE27 <potassiumE19@gmail.com>
2024-02-27 20:31:08 -0700

    Made Enemy abstract. Created FlyingTestEnemy for basic enemy testing.

--- Assets/Entities/Enemies/Enemy/Enemy.cs
+++ Assets/Entities/Enemies/Enemy/Enemy.cs
@@ -2,24 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public Collider2D collider;
 	public float health = 10.0f;
     public int steamOnHit = 50;
 
-    public Vector2 moveTarget;
-    public bool isTargeting = false;
-    public float NOTICE_RANGE = 10.0f;
+	public Vector2 moveTarget;
+	public bool isTargeting = false;
+	public float NOTICE_RANGE = 10.0f;
 
-    public float MAX_MOVEMENT_SPEED = 0.5f; //m per second
-    private EntityMover mover;
+    protected EntityMover mover;
 
-    private float kbStrength = 0.8f;
-    private int kbDurationLeft = 0;
+    protected float kbStrength = 0.8f;
+    protected int kbDurationLeft = 0;
     public readonly int KB_DURATION_FRAMES = 20;
-    private Vector2 kbDirectionalBias = new Vector2(1, 0);
+    protected Vector2 kbDirectionalBias = new Vector2(1, 0);
 
     int count = 0;
 
@@ -32,18 +30,7 @@ public class Enemy : MonoBehaviour
     void FixedUpdate()
     {
         mover.persistentVel *= 0; //reset velocity
-        if (isTargeting)
-            SetVelocityTowardTarget(moveTarget);
-
-        if (count % 100 < 50)
-		{
-			mover.persistentVel += new Vector2(0, 0.5f);
-		}
-        else
-		{
-			mover.persistentVel += new Vector2(0, -0.5f);
-        }
-
+        
 		count++;
         if (kbDurationLeft < 1)
             mover.constantVels["kbVelocity"] *= 0;
@@ -67,21 +54,4 @@ public class Enemy : MonoBehaviour
 		mover.constantVels["kbVelocity"] = direction.normalized * kbStrength * kbDirectionalBias;
         kbDurationLeft = KB_DURATION_FRAMES; 
 	}
-
-    /// <summary>
-    /// Sets velocity to point toward a target position in world-space.
-    /// Consider adding momentum system in the future so that enemies respond to moving targets in a smoother way
-    /// </summary>
-    /// <param name="target"></param>
-    private void SetVelocityTowardTarget(Vector2 target)
-    {
-        Vector2 targetDir = target - (Vector2)rb.transform.position;
-		float approachSlowFactor = 1.0f;
-        if (targetDir.sqrMagnitude < 0.5)
-            approachSlowFactor = targetDir.sqrMagnitude * 2; //using the squared version so that movement slows less further from the target.
-			
-        targetDir.Normalize();
-
-		mover.persistentVel += targetDir * (MAX_MOVEMENT_SPEED * approachSlowFactor);
-    }
 }
---
+++ Assets/FlyingTestEnemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingTestEnemy : Enemy
+{
+	public float MAX_MOVEMENT_SPEED = 0.5f; //m per second
+
+	private new float kbStrength = 1.0f;
+	public new readonly int KB_DURATION_FRAMES = 25;
+
+	int count = 0;
+
+	void Start()
+	{
+		mover = GetComponent<EntityMover>();
+		mover.constantVels.Add("kbVelocity", new Vector2());
+	}
+
+	void FixedUpdate()
+	{
+		mover.persistentVel *= 0; //reset velocity
+		if (isTargeting)
+			SetVelocityTowardTarget(moveTarget);
+
+		if (count % 100 < 50)
+		{
+			mover.persistentVel += new Vector2(0, 0.5f);
+		}
+		else
+		{
+			mover.persistentVel += new Vector2(0, -0.5f);
+		}
+
+		count++;
+		if (kbDurationLeft < 1)
+			mover.constantVels["kbVelocity"] *= 0;
+		else
+			kbDurationLeft--;
+	}
+
+	/// <summary>
+	/// Sets velocity to point toward a target position in world-space.
+	/// Consider adding momentum system in the future so that enemies respond to moving targets in a smoother way
+	/// </summary>
+	/// <param name="target"></param>
+	private void SetVelocityTowardTarget(Vector2 target)
+	{
+		Vector2 targetDir = target - (Vector2)rb.transform.position;
+		float approachSlowFactor = 1.0f;
+		if (targetDir.sqrMagnitude < 0.5)
+			approachSlowFactor = targetDir.sqrMagnitude * 2; //using the squared version so that movement slows less further from the target.
+
+		targetDir.Normalize();
+
+		mover.persistentVel += targetDir * (MAX_MOVEMENT_SPEED * approachSlowFactor);
+	}
+}
