83da2f7c5710f399105369bb6c0353d10f4ed21b
CobaltE27 <potassiume19@gmail.com>
2024-03-06 13:48:31 -0700

    Moved parts of IFlier that are really shared by all moving entities to IMoving (all at this point.)

--- Assets/Entities/Enemies/CapabilityInterfaces/IFlier.cs
+++ Assets/Entities/Enemies/CapabilityInterfaces/IFlier.cs
@@ -2,16 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IFlier
+public interface IFlier: IMoving
 {
-	/// <summary>
-	/// Method that can be called each frame as the flier move toward its target
-	/// </summary>
-	/// <param name="speedMultiplier">Multiplier on this enemy's base speed.</param>
-	void MoveTowardTarget(float speedMultiplier);
-
-    /// <summary>
-    /// Coroutine which makes the enemy move idly. Should be stopped when puppeted.
-    /// </summary>
-    IEnumerator Idle();
+	
 }
---
+++ Assets/Entities/Enemies/CapabilityInterfaces/IMoving.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMoving
+{
+	/// <summary>
+	/// Method that can be called each frame as the flier move toward its target
+	/// </summary>
+	/// <param name="speedMultiplier">Multiplier on this enemy's base speed.</param>
+	void MoveTowardTarget(float speedMultiplier);
+
+	/// <summary>
+	/// Coroutine which makes the enemy move idly. Should be stopped when puppeted.
+	/// </summary>
+	IEnumerator Idle();
+}
