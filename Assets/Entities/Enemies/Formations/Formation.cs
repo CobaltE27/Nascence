2e801c1057276e596a4b027b394eb2523f3ce08d
CobaltE27 <potassiume19@gmail.com>
2024-03-18 20:18:07 -0600

    Began implementation of HaloLineFormation, though Formation details must still be resolved.

--- Assets/Entities/Enemies/Formations/Formation.cs
+++ Assets/Entities/Enemies/Formations/Formation.cs
@@ -8,11 +8,16 @@ public abstract class Formation
     Vector2 puppeteerCenter; //this should usually be the player, but may lag behind them or be the center of a room or something
     private Vector2 FormationCenter { get { return puppeteerCenter + displacementFromCenter; } }//Where the center/beginning of this formation is
     private Vector2 displacementFromCenter;
-    List<Tuple<Enemy, Vector2>> positions;
+    private List<Tuple<Enemy, Vector2>> positions; //consider whether this is really the best way to store these, or whether there should be two seperate lists.
 
     public Formation(ref Vector2 puppeteerCenter, ref Vector2 displacementFromCenter)
     {
         this.puppeteerCenter = puppeteerCenter;
         this.displacementFromCenter = displacementFromCenter;
+        positions = new List<Tuple<Enemy, Vector2>>();
 	}
+
+    public abstract void AddPuppet(Enemy puppet);
+
+    public abstract void RemovePuppet(Enemy puppet);
 }
---
+++ Assets/Entities/Enemies/Formations/HaloLineFormation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HaloLineFormation : Formation
+{
+    public HaloLineFormation(ref Vector2 puppeteerCenter, ref Vector2 displacementFromCenter) : base(ref puppeteerCenter, ref displacementFromCenter)
+    {
+
+    }
+}
