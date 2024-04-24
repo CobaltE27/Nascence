9d12371b5c75950c3a3d03f4c93880a39914b296
CobaltE27 <potassiume19@gmail.com>
2024-04-23 19:55:02 -0600

    Finished implementation for abstract Puppetteer class

--- Assets/Entities/Character/Puppetteer.cs
+++ Assets/Entities/Character/Puppetteer.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Puppetteer : MonoBehaviour
 {
-    protected List<Formation> formations = new List<Formation>();
-    //protected Queue???<functors/coroutines> groupAttacks;
+    protected List<Formation> formations = new();
+    protected List<GroupAttack> groupAttacks = new();
 
-    void Start()
+    protected delegate IEnumerator GroupAttack(Action callBack);
+
+    protected virtual void Start()
     {
-        
+        //initialize formations, group attack, groupAttacks list, etc.
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Calls CleanFormations()
+    /// </summary>
     protected virtual void FixedUpdate()
     {
         CleanFormations();
@@ -43,4 +48,9 @@ public abstract class Puppetteer : MonoBehaviour
                 if (puppet == null)
                     form.PuppetDied();
     }
+
+    /// <summary>
+    /// Coroutine that decides what to do between states. In many cases, will not need to execute over multiple updates, but can as a coroutine.
+    /// </summary>
+    protected abstract IEnumerator DecideNextState();
 }
