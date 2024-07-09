6411daa17855a43dfad476526d8161afd5075a74
CobaltE27 <potassiume19@gmail.com>
2024-07-08 19:10:49 -0600

    Ensured fastfall can't reduce your falling speed

--- Assets/Entities/Character/CharMovement.cs
+++ Assets/Entities/Character/CharMovement.cs
@@ -241,7 +241,7 @@ public class CharMovement : EntityMovement
                 mover.persistentVel.x *= -MAX_SPEED * 0.5f;
             }
             else
-                mover.persistentVel.y = MAX_FALL / 2;
+                mover.persistentVel.y = Mathf.Min(MAX_FALL / 2, mover.persistentVel.y);
             downKeyPressed = false;
         }
 
