f6188077be13a531f23783f25415f22781a5006d
CobaltE27 <potassiume19@gmail.com>
2024-07-08 22:04:23 -0600

    Fixed bug which disabled jumping after running into a wall

--- Assets/Entities/EntityMover.cs
+++ Assets/Entities/EntityMover.cs
@@ -7,6 +7,7 @@ public class EntityMover : MonoBehaviour
     public CollisionCalculator collCalc;
     public Vector2 persistentVel = new Vector2();
 	public Dictionary<string, Vector2> constantVels = new Dictionary<string, Vector2>();
+    public float minimumSpeed = 0.01f;
 
     void Start()
     {
@@ -15,7 +16,12 @@ public class EntityMover : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 displacement = collCalc.MoveAndSlideRedirectVelocity(ref persistentVel, Time.deltaTime);
+        if (Mathf.Abs(persistentVel.x) <= minimumSpeed)
+            persistentVel.x = 0;
+		if (Mathf.Abs(persistentVel.y) <= minimumSpeed)
+			persistentVel.y = 0;
+
+		Vector2 displacement = collCalc.MoveAndSlideRedirectVelocity(ref persistentVel, Time.deltaTime);
         foreach (Vector2 vel in constantVels.Values)
             displacement += collCalc.MoveAndSlide(vel, Time.deltaTime);
 
